26eddf4fd1a22c612620bf89435699d298909737
qianjun <er6788@126.com>
2012-01-11 14:06:16 +0800

    qianjun

--- app/controllers/study_plans_controller.rb
+++ app/controllers/study_plans_controller.rb
@@ -19,7 +19,7 @@ class StudyPlansController < ApplicationController
   def plan_status
     days=UserPlanRelation.find_by_user_id(cookies[:user_id].to_i)
     day_all={}
-    which_day=1
+    which_day=0
     practise=0
     exercise=0
     month_action={}
@@ -45,35 +45,28 @@ class StudyPlansController < ApplicationController
       actions.each do |action|
         month_action["#{action.types}_#{action.created_at.to_datetime.day}"]=action.total_num
       end unless actions.blank?
-      tasks=PlanTask.find_by_sql("select task_types,num,created_at from plan_tasks where study_plan_id=#{days.study_plan_id} and
+      tasks=PlanTask.find_by_sql("select task_types,num from plan_tasks where study_plan_id=#{days.study_plan_id} and
             period_types=#{PlanTask::PERIOD_TYPES[:EVERYDAY]} and task_types in (#{PlanTask::TASK_TYPES[:PRACTICE]},#{PlanTask::TASK_TYPES[:RECITE]})")
       unless tasks.blank?
         tasks.each do |task|
-          task_num["#{task.task_types}_#{task.created_at.to_datetime.day}"]=task.num
+          task_num["#{task.task_types}"]=task.num
         end
-        practise=task_num["#{PlanTask::TASK_TYPES[:PRACTICE]}_#{Time.now.day}"]
-        exercise=task_num["#{PlanTask::TASK_TYPES[:RECITE]}_#{Time.now.day}"]
+        practise=task_num["#{PlanTask::TASK_TYPES[:PRACTICE]}"]
+        exercise=task_num["#{PlanTask::TASK_TYPES[:RECITE]}"]
       end
       day_status={}
       unless day_all[params[:end].to_datetime.month].blank?
         which_day=day_all[params[:end].to_datetime.month].index(Time.now.day).nil? ? 0 :day_all[params[:end].to_datetime.month].index(Time.now.day)+1
         day_all[params[:end].to_datetime.month].each do |day_task|
           status=false
-          [PlanTask::TASK_TYPES[:PRACTICE],PlanTask::TASK_TYPES[:RECITE]].each do |types|
-            if task_num["#{types}_#{day_task}"].nil?
-              status=true
-              break
-            else
-              if (!month_action["#{types}_#{day_task}"].nil?  and month_action["#{types}_#{day_task}"]== task_num["#{types}_#{day_task}"])
-                status=true
-              else
-                status=false
-                break
-              end
-            end
+          if !month_action["#{PlanTask::TASK_TYPES[:PRACTICE]}_#{day_task}"].nil? and !month_action["#{PlanTask::TASK_TYPES[:RECITE]}_#{day_task}"].nil? and
+              month_action["#{PlanTask::TASK_TYPES[:PRACTICE]}_#{day_task}"]== task_num["#{PlanTask::TASK_TYPES[:PRACTICE]}"] and month_action["#{PlanTask::TASK_TYPES[:RECITE]}_#{day_task}"]== task_num["#{PlanTask::TASK_TYPES[:RECITE]}"]
+            status=true
+          else
+            status=false
           end
           day_status[day_task]=status
-        end      
+        end
       end
     end
     respond_to do |format|
