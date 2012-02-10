3bc8590f2febbe8e732f66579cd764b446896be0
qianjun <er6788@126.com>
2012-02-10 14:57:21 +0800

    qianjun

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,5 @@ class Order< ActiveRecord::Base
   STATUS = {:NOMAL => 1, :INVALIDATION => 0} #1 正常  0 失效
   TYPES = {:CHARGE => 1, :OTHER => 0,:TRIAL_SEVEN => 2, :ACCREDIT => 3,:COMPETE => 4} #1 付费  0 其它 2 七天试用
   TYPE_NAME = {1 => "付费", 0 => "其它",2=>"七天试用",3=>"授权码",4=>"模考大赛"}
-
   USER_ORDER = {:VIP => 0, :TRIAL => 1, :NOMAL => 2} #根据order类型和状态判断当前用户的情况：0 vip  1 试用用户  2 普通用户
 end
