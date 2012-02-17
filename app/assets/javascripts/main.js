c92c1dc678eaa4a826eb3ff55a19032356a190d5
lifang <santhe_2004@126.com>
2012-02-17 13:41:09 +0800

    update collections

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -220,5 +220,23 @@ function reset_exam() {
     window.open("/simulations/" + sim_id + "/reset_exam");
 }
 
+// 自动轮换内容
+$(document).ready(function(){
+    var objStr = ".change ul li";
+    $(objStr + ":not(:first)").css("display","none");
+    setInterval(function(){
+        if( $(objStr + ":last").is(":visible")){
+            $(objStr + ":first").fadeIn("slow").addClass("in");
+            $(objStr + ":last").hide()
+        }
+        else{
+            $(objStr + ":visible").addClass("in");
+            $(objStr + ".in").next().fadeIn("slow");
+            $(objStr + ".in").hide().removeClass("in")
+            }
+    },4000) //每3秒钟切换
+})
+
+
 
 
