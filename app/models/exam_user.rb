cc975a8fd77b3351e9025335fb850c95ae15e191
lifang <santhe_2004@126.com>
2012-03-13 16:57:55 +0800

    update detial

--- app/models/exam_user.rb
+++ app/models/exam_user.rb
@@ -1,9 +1,7 @@
 # encoding: utf-8
 class ExamUser < ActiveRecord::Base
   belongs_to :user
-  #has_many :rater_user_relations,:dependent=>:destroy
   belongs_to :examination
-  #has_many :exam_raters,:through=>:rater_user_relations,:foreign_key=>"exam_rater_id"
   belongs_to :paper
   require 'rexml/document'
   include REXML
